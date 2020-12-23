1e23adc6593e5be062a8d0deb5e785ccbc0c32d4
taheer-shaikh <taheer.shaikh@ecosmob.com>
2020-12-23 14:10:20 +0530

    edited 3 files

--- flutter_app/lib/Pickers.dart
+++ flutter_app/lib/Pickers.dart
@@ -6,7 +6,6 @@ class PickerDemo extends StatefulWidget {
 }
 
 class _PickerDemoState extends State<PickerDemo> {
-
   DateTime _dateTime;
   DateTime _todays;
 
@@ -29,48 +28,59 @@ class _PickerDemoState extends State<PickerDemo> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Today's Date ${_todays.day}/${_todays.month}/${_todays.year}",style: TextStyle(
-                fontSize: 25,
-              color: Colors.purple.shade500
-            ),),
-            SizedBox(height: 50,),
-            Text(_dateTime == null ? "Nothing is Selected" : ("Your Birthdate ${_dateTime.day}/${_dateTime.month}/${_dateTime.year}"),style: TextStyle(
-              fontSize: 25,
-              color: Colors.purple.shade500
-            ),),
-            SizedBox(height: 35,),
+            Text(
+              "Today's Date ${_todays.day}/${_todays.month}/${_todays.year}",
+              style: TextStyle(fontSize: 25, color: Colors.purple.shade500),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              _dateTime == null
+                  ? "Nothing is Selected"
+                  : ("Your Birthdate ${_dateTime.day}/${_dateTime.month}/${_dateTime.year}"),
+              style: TextStyle(fontSize: 25, color: Colors.purple.shade500),
+            ),
+            SizedBox(
+              height: 35,
+            ),
             FlatButton(
-              color: Colors.purple.shade200,
-                onPressed: (){
-                  showDatePicker(context: context,
-                      initialDate: DateTime.now(),
-                      firstDate: DateTime(1950),
-                      lastDate:DateTime(2050)).then((date){
-                        setState(() {
-                          _dateTime = date;
-                        });
+                color: Colors.purple.shade200,
+                onPressed: () {
+                  showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(1950),
+                          lastDate: DateTime(2050))
+                      .then((date) {
+                    setState(() {
+                      _dateTime = date;
+                    });
                   });
                 },
-                child:Text("Select Birth DatePicker")),
-            SizedBox(height: 30,),
+                child: Text("Select Birth DatePicker")),
+            SizedBox(
+              height: 30,
+            ),
             age(selecteddate: _dateTime)
           ],
         ),
       ),
     );
   }
-  Widget age({DateTime selecteddate}){
-    if(selecteddate == null)
-      {
-        return Text("");
-      }
-    if(selecteddate != null) {
-      return Text(/*"${selecteddate == null ? "Nothing is Selected yet" :*/" Your age is ${(_todays.year)-(_dateTime.year)} Years ",
-      style: TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 21,
-        color: Colors.purpleAccent.shade700
-      ),);
+
+  Widget age({DateTime selecteddate}) {
+    if (selecteddate == null) {
+      return Text("");
+    }
+    if (selecteddate != null) {
+      return Text(
+        /*"${selecteddate == null ? "Nothing is Selected yet" :*/ " Your age is ${(_todays.year) - (_dateTime.year)} Years ",
+        style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 21,
+            color: Colors.purpleAccent.shade700),
+      );
     }
   }
 }
